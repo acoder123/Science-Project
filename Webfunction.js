1c0cd87a07111a8e5d8dc11aa695fe4d2161ff4b
acoder123 <74914589+acoder123@users.noreply.github.com>
2021-01-13 10:22:40 +0200

    Add files via upload

---
+++ Webfunction.js
@@ -0,0 +1,9 @@
+function visible(){
+    document.getElementById("hidden").style.visibility = 'visible';
+    document.getElementById("hidden").innerHTML = 'More Calcium Less Fat ';
+    
+}
+function unVisible(){
+    document.getElementById("hidden").style.visibility = 'hidden';
+    document.getElementById("hidden").innerHTML = '';
+}
